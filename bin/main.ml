55b66c9430ffc8fc396dc99f8ece8f0abc99da2a
jdc395 <jdc395d@cornell.edu>
2026-04-21 12:08:03 -0400

    Implement Phase 2 walking skeleton
    - Wrote level ASCII parser with spawn extraction (level.ml)
    - Wrote AABB swept collision physics (physics.ml)
    - Wrote player step: input, gravity, fixed-height jump (player.ml)
    - Wrote tile and player renderer (render.ml)
    - Added OUnit2 tests for level parser and physics sweep

--- bin/main.ml
+++ bin/main.ml
@@ -3,11 +3,12 @@ open Fwg
 let () =
   Gfx.init ~width:800 ~height:600 ~title:"Fireboy & Watergirl";
   Gfx.set_target_fps 60;
-  let bg = Gfx.rgb 20 22 38 in
+  let state = ref (Game.init Level.test_level) in
   while not (Gfx.should_close ()) do
     Gfx.begin_frame ();
-    Gfx.clear bg;
-    Gfx.draw_text "fwg" ~x:12 ~y:10 ~size:20 Gfx.white;
+    let inp = Input.poll () in
+    state := Game.update !state inp (1.0 /. 60.0);
+    Render.draw !state;
     Gfx.end_frame ()
   done;
   Gfx.shutdown ()
--- test/test_vec2.ml
+++ test/test_vec2.ml
@@ -1,48 +1,46 @@
 open Fwg
+open OUnit2
 
 let eps = 1e-9
+let aeq msg expected actual =
+  assert_equal ~msg ~cmp:(fun a b -> abs_float (a -. b) < eps) expected actual
 
-let check_float msg expected actual =
-  Alcotest.(check (float eps)) msg expected actual
-
-let test_zero () =
+let test_zero _ =
   let v = Vec2.zero in
-  check_float "zero x" 0.0 v.x;
-  check_float "zero y" 0.0 v.y
+  aeq "zero x" 0. v.x;
+  aeq "zero y" 0. v.y
 
-let test_add () =
+let test_add _ =
   let a = { Vec2.x = 1.5; y = -2.0 } in
-  let b = { Vec2.x = 2.5; y = 4.0 } in
+  let b = { Vec2.x = 2.5; y =  4.0 } in
   let v = Vec2.add a b in
-  check_float "add x" 4.0 v.x;
-  check_float "add y" 2.0 v.y
+  aeq "add x" 4.0 v.x;
+  aeq "add y" 2.0 v.y
 
-let test_sub () =
+let test_sub _ =
   let a = { Vec2.x = 5.0; y = 1.25 } in
-  let b = { Vec2.x = 3.0; y = 2.0 } in
+  let b = { Vec2.x = 3.0; y = 2.0  } in
   let v = Vec2.sub a b in
-  check_float "sub x" 2.0 v.x;
-  check_float "sub y" (-0.75) v.y
+  aeq "sub x"    2.0  v.x;
+  aeq "sub y" (-0.75) v.y
 
-let test_scale () =
-  let v = { Vec2.x = -3.0; y = 2.0 } in
+let test_scale _ =
+  let v   = { Vec2.x = -3.0; y = 2.0 } in
   let out = Vec2.scale 2.5 v in
-  check_float "scale x" (-7.5) out.x;
-  check_float "scale y" 5.0 out.y
+  aeq "scale x" (-7.5) out.x;
+  aeq "scale y"   5.0  out.y
 
-let test_length () =
+let test_length _ =
   let v = { Vec2.x = 3.0; y = 4.0 } in
-  check_float "length" 5.0 (Vec2.length v)
+  aeq "length" 5.0 (Vec2.length v)
 
-let () =
-  Alcotest.run "vec2"
-    [
-      ( "ops",
-        [
-          Alcotest.test_case "zero" `Quick test_zero;
-          Alcotest.test_case "add" `Quick test_add;
-          Alcotest.test_case "sub" `Quick test_sub;
-          Alcotest.test_case "scale" `Quick test_scale;
-          Alcotest.test_case "length" `Quick test_length;
-        ] );
-    ]
+let suite =
+  "vec2" >::: [
+    "zero"   >:: test_zero;
+    "add"    >:: test_add;
+    "sub"    >:: test_sub;
+    "scale"  >:: test_scale;
+    "length" >:: test_length;
+  ]
+
+let () = run_test_tt_main suite
